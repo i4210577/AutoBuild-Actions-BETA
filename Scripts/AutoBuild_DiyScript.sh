081d59b6d84dc6888f196643083dda31769ea6c7
i4210577 <90830668+i4210577@users.noreply.github.com>
2022-11-25 04:24:34 -0800

    Update AutoBuild_DiyScript.sh

--- Scripts/AutoBuild_DiyScript.sh
+++ Scripts/AutoBuild_DiyScript.sh
@@ -66,7 +66,8 @@ EOF
 		AddPackage git themes luci-theme-argon jerrykuku 18.06
 		AddPackage svn apps minieap immortalwrt/packages/branches/openwrt-18.06/net
 		#AddPackage svn other luci-app-openclash vernesong/OpenClash/branches/dev
-                AddPackage git other openclash vernesong dev
+                #AddPackage git other openclash vernesong dev
+                AddPackage git other openclash vernesong master
 		AddPackage git lean luci-app-argon-config jerrykuku master
 		AddPackage git other luci-app-ikoolproxy iwrt main
 		AddPackage git other helloworld fw876 master
